aab20801057b590a7481af778b0d84cff30a7da4
andres alvarez <mail>
2026-02-11 18:59:48 -0500

    header

--- web/themes/custom/prosubastas/build/js/main.script.js
+++ web/themes/custom/prosubastas/build/js/main.script.js
@@ -8276,6 +8276,19 @@ __webpack_require__.r(__webpack_exports__);
 // extracted by mini-css-extract-plugin
 
 
+/***/ }),
+
+/***/ "./components/header/header.scss":
+/*!***************************************!*\
+  !*** ./components/header/header.scss ***!
+  \***************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
 /***/ })
 
 /******/ 	});
@@ -8397,6 +8410,7 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 		var installedChunks = {
 /******/ 			"/build/js/main.script": 0,
+/******/ 			"components/header/header": 0,
 /******/ 			"build/css/main.style": 0
 /******/ 		};
 /******/ 		
@@ -8449,8 +8463,9 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["build/css/main.style"], function() { return __webpack_require__("./src/js/main.script.js"); })
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["build/css/main.style"], function() { return __webpack_require__("./src/scss/main.style.scss"); })
+/******/ 	__webpack_require__.O(undefined, ["components/header/header","build/css/main.style"], function() { return __webpack_require__("./src/js/main.script.js"); })
+/******/ 	__webpack_require__.O(undefined, ["components/header/header","build/css/main.style"], function() { return __webpack_require__("./src/scss/main.style.scss"); })
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["components/header/header","build/css/main.style"], function() { return __webpack_require__("./components/header/header.scss"); })
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
